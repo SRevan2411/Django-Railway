01d84d0ee19c4f96201ea4a200d4ba9ec5cfab9c
SRevan2411 <104152947+SRevan2411@users.noreply.github.com>
2024-10-22 19:57:24 -0600

    Added-CourseViews

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,10 +3,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from .serializers import CourseSerializer
+from .models import Course
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
 # Create your views here.
 
@@ -46,3 +49,12 @@ class GetUserProfileView(APIView):
     def get(self,request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+    
+
+class CourseCreateView(generics.CreateAPIView):
+    serializer_class = CourseSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
