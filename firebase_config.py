351b79a211329fc71d9b6d1fbcab3588517330d1
Eduardo Rafael Pérez Flores <104152947+SRevan2411@users.noreply.github.com>
2025-09-24 00:09:26 -0600

    Delete firebase_config.py

--- firebase_config.py
+++
@@ -1,9 +0,0 @@
-# firebase_config.py
-import firebase_admin
-from firebase_admin import credentials, storage
-
-# Ruta al archivo JSON
-cred = credentials.Certificate("reactvideos-291d3-firebase-adminsdk-6p17c-91ce80f2c3.json")
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'gs://reactvideos-291d3.appspot.com'
-})
